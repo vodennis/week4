d91fdeb8c88ca61f7dac5c8c581fc9d04d9d681c
vodennis <dvoburris@gmail.com>
2024-09-20 19:05:03 -0500

    Update files

--- app/Gallery.tsx
+++ app/Gallery.tsx
@@ -1,29 +1,27 @@
-"use client";  // Mark this component as a Client Component
-
 import React, { useState } from 'react';
 
 interface GalleryProps {
-images: string[];
+    images: string[];
 }
 
 const Gallery: React.FC<GalleryProps> = ({ images }) => {
-const [currentImage, setCurrentImage] = useState(0);
+    const [currentImage, setCurrentImage] = useState(0);
 
-const nextImage = () => {
+    const nextImage = () => {
     setCurrentImage((prevImage) => (prevImage + 1) % images.length);
-};
+    };
 
-const prevImage = () => {
+    const prevImage = () => {
     setCurrentImage((prevImage) => (prevImage === 0 ? images.length - 1 : prevImage - 1));
-};
+    };
 
-return (
-    <div>
-    <button onClick={prevImage}>Previous</button>
-    <img src={images[currentImage]} alt={`Image ${currentImage}`} />
-    <button onClick={nextImage}>Next</button>
-    </div>
-);
+    return (
+        <div>
+        <button onClick={prevImage}>Previous</button>
+        <img src={images[currentImage]} alt={`Image ${currentImage}`} />
+        <button onClick={nextImage}>Next</button>
+        </div>
+    );
 };
 
 export default Gallery;
